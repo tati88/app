a80fe519a0b5881679e3ff9ad8d26c71b4a85d81
tati88 <tatevik88@gmail.com>
2016-08-14 20:15:24 +0200

    changed the vies, added devise, lesson 19

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #note to put the actual host
+   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
