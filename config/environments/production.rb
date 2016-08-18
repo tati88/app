5c83fd39aaf7fac08d81b3c08c2d0a30a2778338
tati88 <tatevik88@gmail.com>
2016-08-18 21:27:17 +0200

    production.rb set amazon region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,13 +83,14 @@ Rails.application.configure do
 
  
 #set paperclip upload images to heroku
-    config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
+config.paperclip_defaults = {
+:storage => :s3,
+:s3_credentials => {
+:s3_region => 'us-east-1',
+:bucket => ENV['S3_BUCKET_NAME'],
+:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+}
 }
 
 end
